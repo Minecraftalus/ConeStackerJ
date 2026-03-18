62f443fc9e08083a569a34388b10f03c43758416
Minecraftalus <frenzy961@gmail.com>
2026-03-17 17:36:30 -0700

    Add json support to leaderboard

--- src/main/java/net/alus/ConeStackerJServer.java
+++ src/main/java/net/alus/ConeStackerJServer.java
@@ -22,7 +22,7 @@ public class ConeStackerJServer {
         SpringApplication.run(ConeStackerJServer.class);
     }
 
-    @GetMapping("/topten")
+    @GetMapping(path = "/topten", produces = "application/x-protobuf")
     public ScoreOuterClass.ScoreList handleTopTen() {
         Map<String, Integer> scores = Leaderboard.getInstance().getScores();
 
@@ -40,13 +40,44 @@ public class ConeStackerJServer {
         return builder.build();
     }
 
-    @PostMapping(path = "/savescore")
+    @PostMapping(path = "/savescore", consumes = "application/x-protobuf")
     public void handleSaveScore(@RequestBody ScoreOuterClass.Score score) {
         Leaderboard.getInstance().saveScore(score.getScore(), score.getUser());
     }
 
-    @PostMapping(path = "/savescorelist")
+    @PostMapping(path = "/savescorelist", consumes = "application/x-protobuf")
     public void handleSaveScoreList(@RequestBody ScoreOuterClass.ScoreList scoreList) {
+        for(ScoreOuterClass.Score score : scoreList.getScoresList()) {
+            Leaderboard.getInstance().saveScore(score.getScore(), score.getUser());
+        }
+    }
+
+
+    @GetMapping(path = "/toptenjson", produces = "application/json")
+    public ScoreOuterClass.ScoreList handleTopTenJson() {
+        Map<String, Integer> scores = Leaderboard.getInstance().getScores();
+
+        List<Map.Entry<String, Integer>> topTenList = scores.entrySet()
+            .stream()
+            .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+            .limit(10)
+            .toList();
+
+        ScoreOuterClass.ScoreList.Builder builder = ScoreOuterClass.ScoreList.newBuilder();
+
+        for (Map.Entry<String, Integer> score : topTenList) {
+            builder.addScores(ScoreOuterClass.Score.newBuilder().setUser(score.getKey()).setScore(score.getValue()).build());
+        }
+        return builder.build();
+    }
+
+    @PostMapping(path = "/savescorejson", consumes = "application/json")
+    public void handleSaveScoreJson(@RequestBody ScoreOuterClass.Score score) {
+        Leaderboard.getInstance().saveScore(score.getScore(), score.getUser());
+    }
+
+    @PostMapping(path = "/savescorelistjson", consumes = "application/json")
+    public void handleSaveScoreListJson(@RequestBody ScoreOuterClass.ScoreList scoreList) {
         for(ScoreOuterClass.Score score : scoreList.getScoresList()) {
             Leaderboard.getInstance().saveScore(score.getScore(), score.getUser());
         }
